fc9594995211c6e113df0feed99e59961f8cdf51
Ömer Apaydın <omerapaydin90@gmail.com>
2026-01-14 19:39:18 +0300

    Initial Commit

---
+++ AnimasyonluButtonTests/AnimasyonluButtonTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimasyonluButtonTests.swift
+//  AnimasyonluButtonTests
+//
+//  Created by Ömer on 14.01.2026.
+//
+
+import Testing
+@testable import AnimasyonluButton
+
+struct AnimasyonluButtonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
